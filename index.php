1827af27670911615fbb5f2ce86e10d16910c7e1
watchthemoon <anneliesvandebroek@hotmail.com>
2014-03-29 12:43:00 +0100

    Registratie + login

--- index.php
+++ index.php
@@ -6,31 +6,24 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Home</title>
+	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+	<header>
+		<img src="images/cover.jpg" alt="cover">
+	</header>
+<div id="rightside">	
 	<section id="login">
+		<h2>Welkom bij Restaurant Business</h2>
 		<form action="" method="post">
 		<input type="text" name="email" placeholder="Email" />
 		<input type="password" name="password" placeholder="Paswoord" />
 
 		<input type="submit" name="btnLogin" value="Sign in" />
 		</form>
-		
+		<p>Nog geen account? <a href="registratie.php">Registreer</a></p>
 	</section>	
-	
-	<section id="signup">
-	
-		<h2>Nieuw bij Restaurant Business? <span>Registreer nu</span></h2>
-		<form action="" method="post">
-		<input type="text" name="firstname" placeholder="Firstname" />
-		<input type="text" name="name" placeholder="Lastname" />
-		<input type="email" name="email" placeholder="Email" />
-		<input type="text" name="phonenumber" placeholder="Phone number" />
-		<input type="text" name="birthdate" placeholder="Birthdate" />
-		<input type="password" name="password" placeholder="Paswoord" />
-		<input type="submit" name="btnSignup" value="Registreren" />
-		</form>
-		
-	</section>
+
+	</div>
 </body>
 </html>
