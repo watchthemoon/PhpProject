2a301f5ffbbdfb59a89cdb78b7442ec34b7659ab
JanaDV <jana.devoecht@student.Thomasmore.be>
2014-04-25 12:37:57 +0200

    Reservatie page

--- application/views/reserve.php
+++ application/views/reserve.php
@@ -3,8 +3,20 @@
 
 	<section id="home">
 
-		<p>Hier komt een reservatieplatform</p>
+		<h2>Selecteer de tafel(s) waar u wilt reserveren.</h2>
+		<p>1 tafel is 2 personen<p>
 
+			<section id="tafels">
+				<p>Tafel 1</p>
+				<p>Tafel 2</p>
+				<p>Tafel 3</p>
+				<p>Tafel 4</p>
+				<p>Tafel 5</p>
+			</section>	
+			
+			<form action="" method="post">
+				<input type="submit" value="Reserveer"/>
+			</form>
 	</section>
 
 </div>
