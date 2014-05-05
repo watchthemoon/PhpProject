948c4c1b88b2af97e15ab3b656dd08501178c32d
LauraDeWinter <dw_laura@hotmail.com>
2014-05-05 22:25:44 +0200

    iets verwijderd

--- application/views/admin/reservations.php
+++ application/views/admin/reservations.php
@@ -5,19 +5,6 @@
 
 	<section>
 
-		<p>Hier ziet u alle reeds geplaatste reservaties.</p>
-
-		<?php foreach ($query->result() as $row) { ?>
-				<h2><?php echo $row->date; ?></h2>
-				<p><?php echo $row->peoplenr; ?></p>
-               
-                <form action="/admin/menu/delete" method="post">
-                <input type="hidden" name="menuid" value="<?php echo $row->menuid;?>" />
-                <input type="hidden" name="restaurantid" value="<?php echo $row->restaurantid;?>" />
-                <input type="submit" value="Verwijder" class="delete"/>
-				</form>
-<?php } ?>
-
 	</section>
 
 
