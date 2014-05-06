617ecbb4e2393fff103973bf1ac98653c07e49d8
watchthemoon <anneliesvandebroek@hotmail.com>
2014-05-06 11:46:14 +0200

    overzicht res

--- application/views/restaurants.php
+++ application/views/restaurants.php
@@ -4,11 +4,13 @@
 	<section id="restaurants-view">
 		<?php foreach ($query as $row) { ?>
 			<div class="res">
-				<h2><?php echo $row->name; ?></h2>
 				<img src="<?php echo 'upload/restaurants/' . $row->image; ?>" class="resfoto" alt="<?php echo $row->image; ?>">
+				<h2><?php echo $row->name; ?></h2>
+				<h3><?php echo $row->city; ?></h3>
 				<p><?php echo $row->description; ?></p>
-				<a href="<?php echo site_url('restaurants/detail/'.$row->restaurantid.''); ?>">Meer info</a>
+				<a href="<?php echo site_url('restaurants/detail/'.$row->restaurantid.''); ?>" class="btnmeerinfo">Meer info</a>
 			</div>
-		<?php } ?>	
+		<?php } ?>
+		<div class="clear"></div>	
 	</section>
 
